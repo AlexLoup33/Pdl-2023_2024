09b69c94c1013489603c9a7f83ef3f8feb34b535
Lou-Poueyou Alexandre <a.loupoueyou@icloud.com>
2024-01-15 19:24:05 +0100

    [Add]: TP1 - Serveur d'images

---
+++ tp1/backend/src/main/java/pdl/backend/ImageController.java
@@ -0,0 +1,89 @@
+package pdl.backend;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.http.HttpResponse.ResponseInfo;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@RestController
+public class ImageController {
+
+  @Autowired
+  private ObjectMapper mapper;
+
+  private final ImageDao imageDao;
+
+  @Autowired
+  public ImageController(ImageDao imageDao) {
+    this.imageDao = imageDao;
+  }
+
+  @RequestMapping(value = "/images/{id}", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
+  public ResponseEntity<?> getImage(@PathVariable("id") long id) {
+    Optional<Image> img = imageDao.retrieve(id);
+    if (img.isEmpty()){
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(img.get().getData());
+  }
+
+  @RequestMapping(value = "/images/{id}", method = RequestMethod.DELETE)
+  public ResponseEntity<?> deleteImage(@PathVariable("id") long id) {
+    Optional<Image> img = imageDao.retrieve(id);
+    if (img.isEmpty()){
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);  
+    }
+    imageDao.delete(img.get());
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+  @RequestMapping(value = "/images", method = RequestMethod.POST)
+  public ResponseEntity<?> addImage(@RequestParam("file") MultipartFile file,
+      RedirectAttributes redirectAttributes) {
+        Image img;
+        try {
+          img = new Image(file.getOriginalFilename(), file.getBytes());
+          imageDao.create(img);
+        } catch (IOException e) {
+          return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+  @RequestMapping(value = "/images", method = RequestMethod.GET, produces = "application/json; charset=UTF-8")
+  @ResponseBody
+  public ArrayNode getImageList() {
+    ArrayNode nodes = mapper.createArrayNode();
+    List<Image> images = imageDao.retrieveAll();
+    for (Image img : images) {
+      ObjectNode node = mapper.createObjectNode();
+      node.put("name", img.getName());
+      node.put("id", img.getId());
+      nodes.add(node);
+    }
+    return nodes;
+  }
+}
---
+++ tp1/backend/src/main/java/pdl/backend/PdlTd1Application.java
@@ -0,0 +1,13 @@
+package pdl.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdlTd1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdlTd1Application.class, args);
+	}
+
+}
